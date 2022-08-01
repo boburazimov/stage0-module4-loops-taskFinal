84c027e568092f99bb48a6ff1bcf4c721bcf1b25
bobur <uzbek.4103@gmail.com>
2022-08-01 15:33:22 +0500

    Final Task v1.0

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int row = 0; row < cathetusLength; row++) {
+            for (int col = 1; col <= cathetusLength; col++) {
+                if (col >= cathetusLength - row){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,9 +3,27 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
 
+        for (int row = 0; row < cathetusLength; row++) {
+            int numIs = row + 1;
+            for (int col = 1; col <= cathetusLength + row; col++) {
+                if (col == cathetusLength - row || (col >= cathetusLength - row && col <= cathetusLength + col) ) {
+                    System.out.print(numIs);
+                    if (numIs == 1 || col >= cathetusLength){
+                        numIs++;
+                    } else {
+                        numIs--;
+                    }
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(9);
+        new Pyramid().printPyramid(6);
+        new Pyramid().printPyramid(0);
     }
 }
